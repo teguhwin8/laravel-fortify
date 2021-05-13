4cb0f238e5bd48de91462a9f2edc286c57980727
Teguh Widodo <teguhwin8@gmail.com>
2021-05-13 12:54:45 +0700

    Focus on laravel auth fortify not laravel auth ui

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify' => true]);
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
